6a878266b1d32c21df5efe360cf63bf946d03456
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-11-14 14:53:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const ageInput = document.querySelector("#age");
+const nameInput = document.querySelector("#name");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener('click', promiseResolve);
+
+function promiseResolve() {
+	let name = nameInput.value
+	let age = ageInput.value;
+let resolved = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+      if(age>=18){
+		  resolve(`Welcome, ${name}. You can vote.`);
+	  }else{
+		  reject(`Oh sorry ${name}. You aren't old enough.`);
+	  }
+	}, 4000)
+})	
+
+	resolved.then((message)=>{
+		alert(message);
+	}).catch((message)=>{
+		alert(message);
+	})
+}
